499bd2e8efab3ea35ee06fe93e3821d75a01428d
Ilia Ermakov <prostokventin2@gmail.com>
2023-02-07 16:49:38 +0300

    оптимизация и результаты запусков

--- hw08-gc/src/main/java/ru/calculator/Summator.java
+++ hw08-gc/src/main/java/ru/calculator/Summator.java
@@ -4,29 +4,33 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Summator {
-    private Integer sum = 0;
-    private Integer prevValue = 0;
-    private Integer prevPrevValue = 0;
-    private Integer sumLastThreeValues = 0;
-    private Integer someValue = 0;
-    private final List<Data> listValues = new ArrayList<>();
+    private int sum = 0;
+    private int prevValue = 0;
+    private int prevPrevValue = 0;
+    private int sumLastThreeValues = 0;
+    private int someValue = 0;
+    private final List<Integer> listValues = new ArrayList<>();
 
     //!!! сигнатуру метода менять нельзя
     public void calc(Data data) {
-        listValues.add(data);
-        if (listValues.size() % 6_600_000 == 0) {
+
+        final int value = data.getValue();
+        final int listSize = listValues.size();
+
+        listValues.add(value);
+        if (listSize % 6_600_000 == 0) {
             listValues.clear();
         }
-        sum += data.getValue();
+        sum += value;
 
-        sumLastThreeValues = data.getValue() + prevValue + prevPrevValue;
+        sumLastThreeValues = value + prevValue + prevPrevValue;
 
         prevPrevValue = prevValue;
-        prevValue = data.getValue();
+        prevValue = value;
 
         for (var idx = 0; idx < 3; idx++) {
-            someValue += (sumLastThreeValues * sumLastThreeValues / (data.getValue() + 1) - sum);
-            someValue = Math.abs(someValue) + listValues.size();
+            someValue += (sumLastThreeValues * sumLastThreeValues / (value + 1) - sum);
+            someValue = Math.abs(someValue) + listSize;
         }
     }
 
