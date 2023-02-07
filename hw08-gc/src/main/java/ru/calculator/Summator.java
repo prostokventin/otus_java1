3c6ff3e5fc0f782a18793434b5a83bedde035068
Ilia Ermakov <prostokventin2@gmail.com>
2023-02-08 00:54:11 +0300

    remove list, add new results

--- hw08-gc/src/main/java/ru/calculator/Summator.java
+++ hw08-gc/src/main/java/ru/calculator/Summator.java
@@ -9,17 +9,15 @@ public class Summator {
     private int prevPrevValue = 0;
     private int sumLastThreeValues = 0;
     private int someValue = 0;
-    private final List<Integer> listValues = new ArrayList<>();
+    private int valuesCount = 0;
 
     //!!! сигнатуру метода менять нельзя
     public void calc(Data data) {
+        int value = data.getValue();
 
-        final int value = data.getValue();
-        final int listSize = listValues.size();
-
-        listValues.add(value);
-        if (listSize % 6_600_000 == 0) {
-            listValues.clear();
+        valuesCount++;
+        if (valuesCount % 6_600_000 == 0) {
+            valuesCount = 0;
         }
         sum += value;
 
@@ -30,7 +28,7 @@ public class Summator {
 
         for (var idx = 0; idx < 3; idx++) {
             someValue += (sumLastThreeValues * sumLastThreeValues / (value + 1) - sum);
-            someValue = Math.abs(someValue) + listSize;
+            someValue = Math.abs(someValue) + valuesCount;
         }
     }
 
