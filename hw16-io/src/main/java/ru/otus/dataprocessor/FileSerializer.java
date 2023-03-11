2a8206d8bd86ad69ad5c401675549625c8cbbdd0
Ilia Ermakov <prostokventin2@gmail.com>
2023-03-12 00:34:17 +0300

    решение дз

--- hw16-io/src/main/java/ru/otus/dataprocessor/FileSerializer.java
+++ hw16-io/src/main/java/ru/otus/dataprocessor/FileSerializer.java
@@ -1,14 +1,33 @@
 package ru.otus.dataprocessor;
 
+import com.google.gson.Gson;
+
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Map;
+import java.util.SortedMap;
+import java.util.TreeMap;
 
 public class FileSerializer implements Serializer {
 
+    private final String filename;
+
     public FileSerializer(String fileName) {
+        this.filename = fileName;
     }
 
     @Override
     public void serialize(Map<String, Double> data) {
         //формирует результирующий json и сохраняет его в файл
+        try (FileWriter file = new FileWriter(filename)) {
+            var gson = new Gson();
+            SortedMap<String, Double> sordedData = new TreeMap(data);
+            gson.toJson(sordedData, file);
+            file.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
     }
 }
--- hw16-io/src/main/java/ru/otus/dataprocessor/ResourcesFileLoader.java
+++ hw16-io/src/main/java/ru/otus/dataprocessor/ResourcesFileLoader.java
@@ -1,16 +1,35 @@
 package ru.otus.dataprocessor;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import ru.otus.model.Measurement;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.Type;
+import java.util.Arrays;
 import java.util.List;
 
 public class ResourcesFileLoader implements Loader {
 
+    private final String filename;
+
     public ResourcesFileLoader(String fileName) {
+        this.filename = fileName;
     }
 
     @Override
     public List<Measurement> load() {
         //читает файл, парсит и возвращает результат
+        try (var resource = new InputStreamReader(ResourcesFileLoader.class.getClassLoader().getResourceAsStream(filename))) {
+            var gson = new Gson();
+            var result = (List<Measurement>) gson.fromJson(resource, new TypeToken<List<Measurement>>(){}.getType());
+            return result;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 }
